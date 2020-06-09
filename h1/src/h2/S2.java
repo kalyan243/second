e8a441439f730ad04e03afebefc07d864e9adeb2
kalyan <avkalyan.chakri@gmail.com>
2020-06-09 12:35:11 -0700

    sample

---
+++ h1/src/h2/S2.java
@@ -0,0 +1,15 @@
+package h2;
+
+public class S2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new S2().m1();
+
+	}
+	void m1()
+	{
+		System.out.println("hai");
+	}
+
+}
